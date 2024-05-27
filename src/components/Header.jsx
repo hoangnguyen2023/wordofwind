39f59ba29d9f4ec3c5393d1793e4b6a104c08dbe
hoangnguyen2023 <hoang.nguyenquoc2018@gmail.com>
2024-05-27 15:52:59 +0700

    upload register

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { NavLink } from 'react-router-dom'
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form'
+import axiosClient from '../api/axiosClient';
 
+
+ 
 
 const Header = () => {
+  const [show, setShow] = useState(false);
+  const [email,setEmail]=useState("");
+  const[password,setPassword]=useState("");
+
+
+
+
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const submit=()=>{
+    const formdata=new FormData();
+    formdata.append("Email",email);
+    formdata.append("Password",password);
+   
+
+    axiosClient.post("user",formdata).then(() =>alert("dfffff"))
+    setShow(false)
+
+}
+
+  const styleobject = { color: "White" }
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Login</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control value={email} onChange={ev => setEmail(ev.target.value)} type="Email" placeholder="Enter email" />         
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control value={password} onChange={ex => setPassword(ex.target.value)} type="Password" placeholder="Password" />
+          </Form.Group>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={submit}>
+            Login
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <nav className="navbar navbar-expand-lg navbar-dark  bg-dark">
         <div className="container-fluid">
           <NavLink className="navbar-brand" to="/Home">Romantic Hunting</NavLink>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -30,17 +82,18 @@ const Header = () => {
                   Register
                 </NavLink>
                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><NavLink className="dropdown-item" to="">Login</NavLink></li>
+                  <li><NavLink className="dropdown-item" to="" onClick={handleShow}>Login</NavLink></li>
                   <li><NavLink className="dropdown-item" to="/Register">Register</NavLink></li>
-                
-                 
                 </ul>
               </li>
             </ul>
-            <form className="d-flex">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
+            <div style={styleobject}>
+              <form className="d-flex-colum "  >
+                <p className=''>WelCome</p>
+                <i className="fa-solid fa-user"></i>
+              </form>
+            </div>
+
           </div>
         </div>
       </nav>
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -2,15 +2,36 @@ import React, { useState } from 'react'
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form'
-import aboume from '../assets/images/album19.jpg'
-
+import about from '../assets/images/album19.jpg'
+import axiosClient from '../api/axiosClient';
 
 const RegisterForm = () => {
 
     const [show, setShow] = useState(false);
+    const [email,setEmail]=useState("");
+    const[password,setPassword]=useState("");
+    const[fullName,setFullName]=useState("");
+    const[address,setAddress]=useState("");
+    const[phone,setPhone]=useState("");
+
+
+
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    const submit=()=>{
+        const formdata=new FormData();
+        formdata.append("Email",email);
+        formdata.append("Password",password);
+        formdata.append("FullName",fullName);
+        formdata.append("Address",address);
+        formdata.append("Phone",phone);
+
+        axiosClient.post("user",formdata).then(() =>alert("dfffff"))
+        setShow(false)
+ 
+    }
 
     return (
 
@@ -26,16 +47,16 @@ const RegisterForm = () => {
 
             </div>
             <div className="card text-white text-center">
-                <img src={aboume} className="card-img" alt="..." style={{width:"100%"}}/>
+                <img src={about} className="card-img" alt="..." style={{ width: "100%" }} />
                 <div className="card-img-overlay">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                     <p className="card-text">Last updated 3 mins ago</p>
                     <Button variant="primary" onClick={handleShow}>
-                    Register Now
-                </Button>
+                        Register Now
+                    </Button>
                 </div>
-               
+
             </div>
 
 
@@ -45,24 +66,37 @@ const RegisterForm = () => {
                 </Modal.Header>
                 <Modal.Body>      <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control value={email} onChange={ev=>setEmail(ev.target.value)} type="Email" placeholder="Enter email" />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
                 </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control value={password} onChange={ex=>setPassword(ex.target.value)} type="Password" placeholder="Password" />
                     </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
+                   
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Full Name</Form.Label>
+                        <Form.Control value={fullName} onChange={ex=>setFullName(ex.target.value)} type="FullName" placeholder="Full Name" />
+                    </Form.Group>
+                   
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Address</Form.Label>
+                        <Form.Control value={address} onChange={ex=>setAddress(ex.target.value)} type="Address" placeholder="Address" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Phone</Form.Label>
+                        <Form.Control value={phone} onChange={ex=>setPhone(ex.target.value)} type="Phone" placeholder="Phone Number" />
                     </Form.Group>
                 </Modal.Body>
+                
+
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
                         Close
                     </Button>
-                    <Button variant="primary" onClick={handleClose}>
+                    <Button variant="primary" onClick={submit}>
                         Save Changes
                     </Button>
                 </Modal.Footer>
